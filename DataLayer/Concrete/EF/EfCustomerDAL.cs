609cba90461a22fe1f7a1624ff8aa2ecb4053867
MertPlc <mertpalacioglu@gmail.com>
2021-10-13 02:06:55 +0300

    RepositoryBase created

---
+++ DataLayer/Concrete/EF/EfCustomerDAL.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationLayer.Entities.Concrete;
+using DataLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Concrete.EF
+{
+    public class EfCustomerDAL
+    {
+    }
+}
